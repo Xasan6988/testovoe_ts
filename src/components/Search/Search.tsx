598c45c54f0af25015c0f76b69f7ad65f707fca2
Atryom Khabibyllin <238136@mail.ru>
2023-07-28 12:34:10 +0300

    add filter to search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { getUsers } from '../../store/actions/app.action';
-import {appSlice} from '../../store/slices/app.slice';
+import { appSlice } from '../../store/slices/app.slice';
 import './Search.scss';
 
 
 const Search: React.FC = (): JSX.Element => {
 
-  const {searchString, perPage, page} = useAppSelector(state => state.app);
+  const { searchString, perPage, page } = useAppSelector(state => state.app);
   const dispatch = useAppDispatch();
 
   const [search, setSearch] = useState(searchString);
+  const [filter, setFilter] = useState<'ASC' | 'DESC' | false>(false);
 
   const keyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
@@ -21,28 +22,41 @@ const Search: React.FC = (): JSX.Element => {
   }
 
   const searchHandler = () => {
-    dispatch(getUsers({searchString: search ? search : "Xasan6988", page, perPage}))
+    dispatch(getUsers({ searchString: search ? search : "Xasan6988", page, perPage, sort: filter }))
+  }
+
+  const filterHandler = (filterValue: typeof filter) => {
+    setFilter(filterValue);
+    dispatch(getUsers({ searchString: search ? search : "Xasan6988", page, perPage, sort: filterValue }))
   }
 
   useEffect(() => {
     setSearch(searchString)
   }, [searchString]);
 
-  return(
-    <div className='Search'>
-      <input
-        className="search_input"
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        onKeyDown={keyDownHandler}
-        placeholder={"Начните вводить текст для поиска (не менее трех символов)"}
-      />
-      <button className="search_button" onClick={() => {
-        dispatch(appSlice.actions.setSearchString(search))
-        searchHandler()
-      }}></button>
+  return (
+    <div className="Search">
+      <div className='search_filed'>
+        <input
+          className="search_input"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={keyDownHandler}
+          placeholder={"Начните вводить текст для поиска (не менее трех символов)"}
+        />
+        <button className="search_button" onClick={() => {
+          dispatch(appSlice.actions.setSearchString(search))
+          searchHandler()
+        }}></button>
+      </div>
+      <div className="search_filters">
+        <button className={`filter_btn${filter === 'ASC' ? '_active' : ''}`} onClick={() => filterHandler('ASC')}>По возрастанию</button>
+        <button className={`filter_btn${filter === 'DESC' ? '_active' : ''}`} onClick={() => filterHandler('DESC')}>По убыванию</button>
+        <button className='filter_btn' onClick={() => filterHandler(false)}>Сбросить фильтры</button>
+      </div>
     </div>
+
   )
 }
 
--- src/store/actions/app.action.ts
+++ src/store/actions/app.action.ts
@@ -8,7 +8,7 @@ export const getUsers = ({searchString, page = 1, perPage = 10, sort = false}: I
   return async (dispatch: AppDispatch) => {
     try {
       dispatch(appSlice.actions.fetching())
-      const res  = await axios.get<{total_count: number, items: IUser[]}>(`https://api.github.com/search/users?q=${searchString}&page=${page}&per_page=${perPage}${sort ? '&sort=true' : ''}`);
+      const res  = await axios.get<{total_count: number, items: IUser[]}>(`https://api.github.com/search/users?q=${searchString}&page=${page}&per_page=${perPage}${sort ? '&sort=repositories' : ''}${sort ? '&order=' + sort : ''}`);
 
       if (sort === 'ASC') {
         dispatch(appSlice.actions.setUsers(res.data.items.reverse()));
